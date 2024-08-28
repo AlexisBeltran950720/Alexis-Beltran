c7cc91e262770a348bf486c250047cbef4a56b7e
AlexisBeltran950720 <alexisbeltran950720@gmail.com>
2024-08-28 12:55:24 -0600

    Create Sumadenumeros.py

---
+++ Ejercicios en python/Sumadenumeros.py
@@ -0,0 +1,5 @@
+#este programa suma dos numeros
+num1=int(input("Ingrese el primer numero\n"))
+num2=int(input("Ingrese el segundo numero\n"))
+suma = num1 + num2
+print(f"La suma de los dos numeros es:{suma}")
